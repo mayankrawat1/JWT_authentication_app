75a6156d7b1687ce117ae94969b83ab310c1880e
mayank <mayank.rawat1@tothenew.com>
2022-10-31 21:51:29 +0530

    add basic frontend using ejs

--- server.js
+++ server.js
@@ -4,11 +4,17 @@ const express = require("express");
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+//middleware
+app.use(express.static("public"));
+
+//view engine
+app.set("view engine", "ejs");
+
+//datbasse connection
 require("./database/connection");
 
-app.get("/", (req, res) => {
-  res.send("hello");
-});
+app.get("/", (req, res) => res.render("home"));
+app.get("/smoothies", (req, res) => res.render("smoothies"));
 
 app.listen(PORT, () => {
   console.log(`listening to the port ${PORT}`);
