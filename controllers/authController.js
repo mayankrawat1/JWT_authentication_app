2406e9a78b32426ce93fa8f5b7e1ee9f386a6159
mayank <mayank.rawat1@tothenew.com>
2022-11-01 11:02:40 +0530

    add user in database

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,5 @@
+const User = require("../database/models/userModel");
+
 module.exports.signup_get = (req, res) => {
   res.render("signup");
 };
@@ -6,8 +8,15 @@ module.exports.login_get = (req, res) => {
   res.render("login");
 };
 
-module.exports.signup_post = (req, res) => {
-  console.log(req.body);
+module.exports.signup_post = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.create({ email, password });
+    res.status(200).send(user);
+  } catch (err) {
+    console.log(err);
+    res.status(400).send(err);
+  }
   res.send("signup");
 };
 
